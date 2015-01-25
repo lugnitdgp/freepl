254eb6ac7263566e0b3b063eca16292e1089f629
Mayank Jha <mayank25080562@gmail.com>
2015-01-25 18:08:47 +0530

    Added email hosts, and port.

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,16 @@ ALLOWED_HOSTS = []
 
 SEND_MAIL = False
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Host for sending e-mail.
+EMAIL_HOST = 'localhost'
+
+# Port for sending e-mail.
+EMAIL_PORT = 1025
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
 # Application definition
 
 INSTALLED_APPS = (
