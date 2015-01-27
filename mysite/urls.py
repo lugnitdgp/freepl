0454e6aef2c9b0680929be853469e71816628d1d
Mayank Jha <mayank25080562@gmail.com>
2015-01-27 12:26:58 +0530

    Bug fix.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from freepl.views import home,authorize,logout_
-from freepl.views import locktheteam,user_verify
+from freepl.views import locktheteam
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,7 +10,5 @@ urlpatterns = patterns('',
    url(r'^dummylocktheteam$', locktheteam),
    url(r'^dummylogout$', logout_),
    url(r'^admin/', include(admin.site.urls)),
-   url(r'^activate/(?P<activation_key>\w+)/$', user_verify),
-   url(r'^captcha/', include('captcha.urls')),
 
 )
