385e8e01360cebe50bf87306d92b34d44dd93fcf
Mayank Jha <mayank25080562@gmail.com>
2015-03-07 14:25:01 +0530

    Updated the media urls.

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from freepl.views import home,authorize,logout_
 from freepl.views import locktheteam
+from mysite import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,5 +11,6 @@ urlpatterns = patterns('',
    url(r'^dummylocktheteam$', locktheteam),
    url(r'^dummylogout$', logout_),
    url(r'^admin/', include(admin.site.urls)),
+   url(r'^media/(.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
 
 )
